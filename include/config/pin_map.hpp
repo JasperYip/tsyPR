715d6b192c43bccdcefcb1694a9fe5e81d58a8a9
jasperyip <51094504+JasperYip@users.noreply.github.com>
2026-05-13 15:42:30 +0100

    updated mapping to fix limit switch

--- include/config/pin_map.hpp
+++ include/config/pin_map.hpp
@@ -48,7 +48,7 @@ constexpr uint32_t BMS_BAUD  = 115200;
 // Limit Switch 
 // =====================
 constexpr uint8_t PIN_FWD_SWH = 9;  // Fwd limit switch
-constexpr uint8_t PIN_BWD_SWH = 10;  // Bwd limit switch
+constexpr uint8_t PIN_BWD_SWH = 19;  // Bwd limit switch
 
 // =====================
 // Hall Effect Sensor
